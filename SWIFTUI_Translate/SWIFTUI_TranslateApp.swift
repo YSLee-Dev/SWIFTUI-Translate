d071492ed80e0338cee26b445e9cddf2e6cdad96
YoonSuLee <ysleedev01@gmail.com>
2023-05-22 19:05:40 +0900

    Initial Commit

---
+++ SWIFTUI_Translate/SWIFTUI_TranslateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWIFTUI_TranslateApp.swift
+//  SWIFTUI_Translate
+//
+//  Created by 이윤수 on 2023/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SWIFTUI_TranslateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
