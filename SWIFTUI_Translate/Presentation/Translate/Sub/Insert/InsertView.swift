d09fdacbd15960dde22feabd322e121f44e69098
YoonSuLee <ysleedev01@gmail.com>
2023-06-05 20:59:10 +0900

    Fix(Translate): InsertView의 텍스트 입력 방법을 TextField로 변경

--- SWIFTUI_Translate/Presentation/Translate/Sub/Insert/InsertView.swift
+++ SWIFTUI_Translate/Presentation/Translate/Sub/Insert/InsertView.swift
@@ -14,33 +14,21 @@ struct InsertView: View {
     
     var body: some View {
         VStack(alignment: .leading){
-            TextEditor(text: self.$tfValue)
+           TextField("번역하고 싶은 문구 입력하세요.", text: $tfValue)
                 .font(.title2)
                 .fontWeight(.semibold)
                 .lineLimit(0)
                 .frame(maxWidth: .infinity,
                        alignment: .leading)
                 .padding(20)
-                .onReceive(Just(self.tfValue)){ data in
-                    self.viewModel.value.send(data)
+                .onSubmit {
+                    print("눌림")
+                    self.viewModel.value.send(tfValue)
                 }
+            
+            Spacer()
         }
         .background(Color(uiColor: UIColor.systemBackground))
         .cornerRadius(10)
-        .overlay{
-            if self.tfValue == ""{
-                VStack{
-                    Text("번역하고 싶은 문구를 입력하세요.")
-                        .font(.title2)
-                        .fontWeight(.semibold)
-                        .foregroundColor(.gray)
-                        .frame(maxWidth: .infinity,
-                               alignment: .leading
-                        )
-                        .padding(EdgeInsets(top: 30, leading: 25, bottom: 0, trailing: 0))
-                    Spacer()
-                }
-            }
-        }
     }
 }
