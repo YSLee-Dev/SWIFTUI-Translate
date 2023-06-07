b1a9539a905b73ed902e2166ebec9c9cabddfd09
YoonSuLee <ysleedev01@gmail.com>
2023-06-07 19:18:41 +0900

    Fix(Translate): 텍스트를 여러줄 입력할 수 있게 TextEditor로 변경 후 확인 버튼 추가

--- SWIFTUI_Translate/Presentation/Translate/Sub/Insert/InsertView.swift
+++ SWIFTUI_Translate/Presentation/Translate/Sub/Insert/InsertView.swift
@@ -13,19 +13,38 @@ struct InsertView: View {
     let viewModel : InsertViewModel
     
     var body: some View {
-        VStack(alignment: .leading){
-           TextField("번역하고 싶은 문구 입력하세요.", text: $tfValue)
+        VStack(alignment: .trailing){
+            Button {
+                self.viewModel.value.send(self.tfValue)
+            } label: {
+                Text("확인")
+                    .font(.callout)
+                    .foregroundColor(.pink)
+                    .padding(20)
+            }
+            
+            TextEditor(text: self.$tfValue)
                 .font(.title2)
                 .fontWeight(.semibold)
                 .lineLimit(0)
                 .frame(maxWidth: .infinity,
                        alignment: .leading)
-                .padding(20)
-                .onSubmit {
-                    print("눌림")
-                    self.viewModel.value.send(tfValue)
+                .padding(EdgeInsets(top: 0, leading: 20, bottom: 20, trailing: 20))
+                .overlay{
+                    if self.tfValue == ""{
+                        VStack{
+                            Text("번역하고 싶은 문구를 입력하세요.")
+                                .font(.title2)
+                                .fontWeight(.semibold)
+                                .foregroundColor(.gray)
+                                .frame(maxWidth: .infinity,
+                                       alignment: .leading
+                                )
+                                .padding(EdgeInsets(top: 10, leading: 25, bottom: 0, trailing: 0))
+                            Spacer()
+                        }
+                    }
                 }
-            
             Spacer()
         }
         .background(Color(uiColor: UIColor.systemBackground))
